21bf072d1f14c4474f0f9a12a881ac534dd51f3c
yao guan shou <281113270@qq.com>
2023-02-21 01:11:28 +0800

    登录注册页面修改

--- client/pages/LogIn/index.js
+++ client/pages/LogIn/index.js
@@ -1,10 +1,8 @@
 import "client/assets/css/base.less";
 import "./index.less";
 
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import { Form, Input, message } from "antd";
+import { Button, Paper, TextField } from "@mui/material";
+import { message } from "antd";
 import { login } from "client/assets/js/request/index";
 import FormItem from "client/component/FormItem";
 import VerificationCode from "client/component/VerificationCode";
@@ -14,30 +12,9 @@ import { checkEmail, checkPassword, checkPhone, checkUser } from "client/utils";
 import { createForm } from "rc-form";
 import React, { useEffect, useState } from "react";
 
-console.log("createForm====", createForm);
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 }
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 }
-};
-
 const Index = (props) => {
   const { pushRoute, routePaths, form } = props;
-  const { getFieldDecorator, getFieldProps, validateFields, getFieldError } =
-    form;
-  /*
-   {
-    name:{
-      value:123,
-      error:"",
-    }
-   }
-
-
-  */
-  const [values, setValues] = useState({});
+  const { validateFields } = form;
 
   const onFinish = async (values) => {
     const {
@@ -61,105 +38,106 @@ const Index = (props) => {
   };
 
   const onSubmit = async () => {
-    validateFields((error, value) => {
-      console.log(error, value);
-      debugger;
+    validateFields((error, values) => {
+      if (!error) {
+        onFinish(values);
+      } else {
+        console.error(error);
+      }
     });
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
   useEffect(() => {}, []);
-  console.log("props==", props);
-  console.log(
-    "error=",
-    getFieldError("name") ? getFieldError("name").join(",") : null
-  );
 
   return (
     <div className="center log-in">
-      <h3>《OT协同办公系统》 </h3>
+      <Paper elevation={3} className="paper">
+        <h3>《OT协同办公系统》 </h3>
 
-      <FormItem
-        className="margin-top-10"
-        rules={[
-          {
-            required: true,
-            message: "请输入用户名或手机号或邮箱"
-          },
-          {
-            validator: (rule, value) => {
-              if (checkUser(value) || checkPhone(value) || checkEmail(value)) {
+        <FormItem
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入用户名或手机号或邮箱"
+            },
+            {
+              validator: (rule, value) => {
+                if (
+                  checkUser(value) ||
+                  checkPhone(value) ||
+                  checkEmail(value)
+                ) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("格式不正确请重新输入");
+              }
+            }
+          ]}
+          label="用户名/手机号/邮箱"
+          form={props.form}
+          name="name">
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入用户名/手机号/邮箱"
+            variant="outlined"
+          />
+        </FormItem>
+
+        <FormItem
+          label="密码"
+          name="password"
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入密码"
+            },
+            {
+              validator: (rule, value) => {
+                if (!checkPassword(value)) {
+                  return Promise.reject(
+                    "密码最少为8位，并且最少含有字母和数字组成"
+                  );
+                }
                 return Promise.resolve();
               }
-              return Promise.reject("格式不正确请重新输入");
             }
-          }
-        ]}
-        label="用户名/手机号/邮箱"
-        form={props.form}
-        name="name">
-        <TextField
-          className="input-w"
-          required
-          placeholder="请输入用户名/手机号/邮箱"
-          variant="outlined"
-        />
-      </FormItem>
+          ]}
+          form={props.form}>
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入密码"
+            variant="outlined"
+            type="password"
+            autoComplete="current-password"
+          />
+        </FormItem>
 
-      <FormItem
-        label="密码"
-        name="password"
-        className="margin-top-10"
-        rules={[
-          {
-            required: true,
-            message: "请输入密码"
-          },
-          {
-            validator: (rule, value) => {
-              if (!checkPassword(value)) {
-                return Promise.reject(
-                  "密码最少为8位，并且最少含有字母和数字组成"
-                );
-              }
-              return Promise.resolve();
-            }
-          }
-        ]}
-        form={props.form}>
-        <TextField
-          className="input-w"
-          required
-          placeholder="请输入密码"
-          variant="outlined"
-          type="password"
-          autoComplete="current-password"
-        />
-      </FormItem>
+        {/*验证码*/}
+        <VerificationCode form={form} />
+        <div className="buttons">
+          <Button
+            className="submit"
+            variant="contained"
+            onClick={() => {
+              onSubmit();
+            }}>
+            登录
+          </Button>
 
-      {/*验证码*/}
-      <VerificationCode form={form} />
-      <div className="buttons">
-        <Button
-          className="submit"
-          onClick={() => {
-            onSubmit();
-          }}>
-          登录
-        </Button>
-
-        <Button
-          variant="contained"
-          className="submit"
-          onClick={() => {
-            pushRoute(routePaths.register);
-          }}>
-          注册
-        </Button>
-      </div>
+          <Button
+            variant="outlined"
+            className="submit"
+            onClick={() => {
+              pushRoute(routePaths.register);
+            }}>
+            注册
+          </Button>
+        </div>
+      </Paper>
     </div>
   );
 };
--- client/pages/Register/index.js
+++ client/pages/Register/index.js
@@ -1,23 +1,22 @@
 import "client/assets/css/base.less";
 import "./index.less";
 
-import { Button, Form, Input, message } from "antd";
+import { Button, Paper, TextField } from "@mui/material";
+import { Form, Input, message } from "antd";
 import { createUser } from "client/assets/js/request/index";
+import FormItem from "client/component/FormItem";
 import VerificationCode from "client/component/VerificationCode";
 import { addRouterApi, toComponent } from "client/router";
 import { checkEmail, checkPassword, checkPhone, checkUser } from "client/utils";
-import React from "react";
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 }
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 }
-};
+import { createForm } from "rc-form";
+import React, { useEffect, useState } from "react";
 
 const Index = (props) => {
-  const { pushRoute, routePaths } = props;
+  const {
+    pushRoute,
+    routePaths,
+    form: { getFieldValue, validateFields }
+  } = props;
   const onFinish = async (values) => {
     await createUser({
       type: 1,
@@ -29,11 +28,191 @@ const Index = (props) => {
     }, 1500);
   };
 
-  const onFinishFailed = () => {};
+  const onSubmit = async () => {
+    validateFields((error, values) => {
+      if (!error) {
+        onFinish(values);
+      } else {
+        console.error(error);
+      }
+    });
+  };
 
   return (
     <div className="center log-in">
-      <h3>《OT协同办公系统》 </h3>
+      <Paper elevation={3} className="paper">
+        <h3>《OT协同办公系统》 </h3>
+
+        <FormItem
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入用户名"
+            },
+            {
+              validator: (rule, value) => {
+                if (checkUser(value)) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("用户名必须最少为4位，并且以字母开头");
+              }
+            }
+          ]}
+          label="用户名"
+          form={props.form}
+          name="name">
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入用户名"
+            variant="outlined"
+          />
+        </FormItem>
+
+        <FormItem
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入手机号"
+            },
+            {
+              validator: (rule, value) => {
+                if (checkPhone(value)) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("请输入正确的手机号码");
+              }
+            }
+          ]}
+          label="手机号"
+          form={props.form}
+          name="phone">
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入手机号"
+            variant="outlined"
+          />
+        </FormItem>
+
+        <FormItem
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入邮箱"
+            },
+            {
+              validator: (rule, value) => {
+                if (checkEmail(value)) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("请输入正确的邮箱");
+              }
+            }
+          ]}
+          label="邮箱"
+          form={props.form}
+          name="email">
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入邮箱"
+            variant="outlined"
+          />
+        </FormItem>
+
+        <FormItem
+          label="密码"
+          name="password"
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入密码"
+            },
+            {
+              validator: (rule, value) => {
+                if (!checkPassword(value)) {
+                  return Promise.reject(
+                    "密码最少为8位，并且最少含有字母和数字组成"
+                  );
+                }
+                return Promise.resolve();
+              }
+            }
+          ]}
+          form={props.form}>
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入密码"
+            variant="outlined"
+            type="password"
+            autoComplete="current-password"
+          />
+        </FormItem>
+
+        <FormItem
+          label="确认密码"
+          name="confirmPassword"
+          className="margin-top-10"
+          rules={[
+            {
+              required: true,
+              message: "请输入密码"
+            },
+            {
+              validator: (rule, value) => {
+                if (!checkPassword(value)) {
+                  return Promise.reject(
+                    "密码最少为8位，并且最少含有字母和数字组成"
+                  );
+                } else if (value && getFieldValue("password") !== value) {
+                  return Promise.reject("输入两次密码不相同，请重新输入");
+                }
+                return Promise.resolve();
+              }
+            }
+          ]}
+          form={props.form}>
+          <TextField
+            className="input-w"
+            required
+            placeholder="请输入密码"
+            variant="outlined"
+            type="password"
+            autoComplete="current-password"
+          />
+        </FormItem>
+
+        <VerificationCode form={props.form} />
+        <div className="buttons">
+          <Button
+            className="submit"
+            variant="contained"
+            onClick={() => {
+              onSubmit();
+            }}>
+            确定
+          </Button>
+          <Button
+            className="submit"
+            variant="outlined"
+            onClick={() => {
+              pushRoute(routePaths.logIn);
+              // historyPush({
+              //   url: routePaths.LogIn
+              // });
+            }}>
+            登录
+          </Button>
+        </div>
+
+        {/*
+
       <Form
         {...layout}
         name="basic"
@@ -124,6 +303,7 @@ const Index = (props) => {
           ]}>
           <Input.Password />
         </Form.Item>
+
         <Form.Item
           label="确认密码"
           name="confirmPassword"
@@ -154,8 +334,8 @@ const Index = (props) => {
           <Input.Password />
         </Form.Item>
 
-        {/*验证码*/}
-        {/* <VerificationCode /> */}
+
+        <VerificationCode />
         <Form.Item {...tailLayout}>
           <div className="buttons">
             <Button
@@ -178,8 +358,10 @@ const Index = (props) => {
           </div>
         </Form.Item>
       </Form>
+    */}
+      </Paper>
     </div>
   );
 };
 
-export default toComponent(addRouterApi(Index));
+export default toComponent(addRouterApi(createForm()(Index)));
