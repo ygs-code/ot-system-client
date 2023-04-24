e25354e606bfd5aea60215b8da56a2f167aff020
yao guan shou <281113270@qq.com>
2023-04-25 01:41:43 +0800

    docker 配置

--- webpack/config/client/webpack.prod.config.js
+++ webpack/config/client/webpack.prod.config.js
@@ -16,6 +16,14 @@ const isDevelopment = NODE_ENV == "development";
 module.exports = {
   mode: "development",
   watch: false,
+
+  output: {
+    // path: path.join(__dirname, 'dist'),
+    // filename:'bundle.js',
+    // 配置 二级目录
+    publicPath: "/client/"
+  },
+
   module: {
     rules: [
       // css
