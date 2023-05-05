c1c32f2766e95d50e943f089021500919b625e33
yao guan shou <281113270@qq.com>
2023-05-06 00:42:38 +0800

    fix: 404页面跳转问题

--- client/component/404/index.js
+++ client/component/404/index.js
@@ -4,6 +4,7 @@ import img from "client/assets/img/404.png";
 import Layout, { layout } from "client/component/Layout";
 import React, { memo } from "react";
 
+const { env: { NODE_ENV, PUBLICPATH, RENDER } = {} } = process;
 export default layout()((props) => {
   const {
     user: { user: { email, id, name, phone, type } = {} } = {},
@@ -19,22 +20,22 @@ export default layout()((props) => {
     setAnchorEl(null);
   };
   return (
-    <div className="container">
-      <img src={img} />
-      <h2>抱歉，您访问的页面出错了</h2>
-      <p>您可能输错了网址，或该网页已删除或不存在</p>
-      <a
-        onClick={(e) => {
-          const { origin } = window.location;
-          console.log("props===", props);
-          console.log("location===", window.location);
+    <div className="no-page">
+      <div className="container">
+        <img src={img} />
+        <h2>抱歉，您访问的页面出错了</h2>
+        <p>您可能输错了网址，或该网页已删除或不存在</p>
+        <a
+          onClick={(e) => {
+            const { origin } = window.location;
 
-          window.location.href = origin;
-          e.stopPropagation();
-        }}
-        className="btn btn-primary btn_blue">
-        返回主页
-      </a>
+            window.location.href = origin + PUBLICPATH;
+            e.stopPropagation();
+          }}
+          className="btn btn-primary btn_blue">
+          返回主页
+        </a>
+      </div>
     </div>
   );
 });
